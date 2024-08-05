6745a45ab6f5847014cf48bd82bdb6dec5353c9d
SimsekBerk <berksamett1@gmail.com>
2024-08-06 02:27:00 +0300

    nft_image and nft_metadata done

--- ts/cluster1/nft_metadata.ts
+++ ts/cluster1/nft_metadata.ts
@@ -17,27 +17,29 @@ umi.use(signerIdentity(signer));
         // Follow this JSON structure
         // https://docs.metaplex.com/programs/token-metadata/changelog/v1.0#json-structure
 
-        // const image = ???
-        // const metadata = {
-        //     name: "?",
-        //     symbol: "?",
-        //     description: "?",
-        //     image: "?",
-        //     attributes: [
-        //         {trait_type: '?', value: '?'}
-        //     ],
-        //     properties: {
-        //         files: [
-        //             {
-        //                 type: "image/png",
-        //                 uri: "?"
-        //             },
-        //         ]
-        //     },
-        //     creators: []
-        // };
-        // const myUri = ???
-        // console.log("Your metadata URI: ", myUri);
+        const image = "https://arweave.net/Ou7QSjf8KPncnlytEZTLnea5sPKFznJ3RmWZPmrg1Ak";
+        const metadata = {
+            name: "Ric's rug",
+            symbol: "RIC",
+            description: "After the rug, builder get blind. All cost is a 5k dollar, that cost include the hospital costs.",
+            image,
+            attributes: [
+                {trait_type: "Seller Ric", value: '5'},
+                {trait_type: "Color", value: 'Purple'}
+            ],
+            properties: {
+                files: [
+                    {
+                        type: "image/png",
+                        uri: image,
+                    },
+                ]
+            },
+            creators: []
+        };
+        const myUri = await umi.uploader.uploadJson(metadata);
+        console.log("URI: ", myUri);
+        // https://arweave.net/-o92lw98_GZni0n5H1hJwZ71hJBNGedw6ERoiWAWr9o
     }
     catch(error) {
         console.log("Oops.. Something went wrong", error);
